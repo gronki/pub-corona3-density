029f9ba44d693ad8e46c97be21551cb88f70d8b3
gronki <gronki@gmail.com>
2018-12-17 19:44:50 +0100

    rysuinki jak w mailu 17 grudnia

--- plotgen/plot.py
+++ plotgen/plot.py
@@ -5,19 +5,31 @@ import matplotlib.pyplot as plt
 import numpy as np
 from maputils import *
 
-ds = read2Ddataset(lambda i,j: 'data/x/' + ix2fn('map1-MR', (i, j)) + '.txt', 30, 40)
+lili = {
+    1: (16.0, 19.25),
+    8: (9.5, 12.5),
+}
 
-fig, ax = plt.subplots(figsize = (8,5))
+for series in ['d', 'x']:
+    for mbhe in [1, 8]:
 
-zz = DPA(ds, 'rho_phot_nh')
-c1 = ax.contourf(par.radii, par.mdots, zz, levels = LogLvl(zz,9), norm = LogNorm())
-ax.set_xscale('log')
-ax.set_yscale('log')
+        ds = read2Ddataset(lambda i,j: 'data/{}/'.format(series) + ix2fn('map{}-MR'.format(mbhe), (i, j)) + '.txt', 30, 40)
 
-ax.set_xlabel('$R / R_{{\\rm schw}}$')
-ax.set_ylabel('$\\dot m$')
-ax.set_title('$n_{{\\rm H}}$ at $\\tau = 2/3$')
+        fig, ax = plt.subplots(figsize = (9,6))
 
-plt.colorbar(c1, ax = ax)
+        zz = np.log10(DPA(ds, 'rho_phot_nh'))
+        ncont = np.int0((lili[mbhe][1] - lili[mbhe][0]) / 0.25 + 1)
+        ll = np.linspace(lili[mbhe][0], lili[mbhe][1], ncont)
+        c1 = ax.contourf(par.radii, par.mdots, zz, ll, cmap = 'jet')
+        ax.set_xscale('log')
+        ax.set_yscale('log')
 
-plt.show()
+        ax.set_xlabel('$R / R_{{\\rm schw}}$')
+        ax.set_ylabel('$\\dot m$')
+        ax.set_title('$\\log n_{{\\rm H}}$ at $\\tau = 2/3$, $M=10^{{{mm}}}$, bil = {se}'
+            .format(mm = mbhe, se = 'dyfu' if series == 'd' else 'coro'))
+
+        plt.colorbar(c1, ax = ax, label = '$\\log n_{{\\rm H}}$', format = '%.1f')
+
+        plt.savefig('map{}{}.png'.format(mbhe, series))
+        plt.close(fig)
